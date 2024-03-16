b8ad997c7071de22392c91580c9975da80215b2e
Bart Milo <bartlomiej.milosz@gmail.com>
2024-03-16 13:12:01 +0100

    Implement BookDao and create tests for create, read one and read all operations

---
+++ src/main/java/io/bartmilo/database/dao/impl/BookDaoImpl.java
@@ -0,0 +1,61 @@
+package io.bartmilo.database.dao.impl;
+
+import io.bartmilo.database.dao.BookDao;
+import io.bartmilo.database.domain.Book;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class BookDaoImpl implements BookDao {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public BookDaoImpl(final JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public void create(Book book) {
+        jdbcTemplate.update(
+                "INSERT INTO books (isbn, title, author_id) VALUES (?, ?, ?)",
+                book.getIsbn(),
+                book.getTitle(),
+                book.getAuthorId()
+        );
+    }
+
+    @Override
+    public Optional<Book> findOne(String isbn) {
+        List<Book> result = jdbcTemplate.query(
+                "SELECT * FROM books WHERE isbn = ? LIMIT 1",
+                new BookRowMapper(),
+                isbn
+        );
+        return result.stream().findFirst();
+    }
+
+    @Override
+    public List<Book> findAll() {
+        return jdbcTemplate.query(
+          "SELECT * FROM books",
+          new BookRowMapper()
+        );
+    }
+
+    public static class BookRowMapper implements RowMapper<Book> {
+        @Override
+        public Book mapRow(ResultSet rs, int rowNum) throws SQLException {
+            return Book.builder()
+                    .isbn(rs.getString("isbn"))
+                    .title(rs.getString("title"))
+                    .authorId(rs.getLong("author_id"))
+                    .build();
+        }
+    }
+}
---
+++ src/test/java/io/bartmilo/database/dao/impl/BookDaoImplTests.java
@@ -0,0 +1,57 @@
+package io.bartmilo.database.dao.impl;
+
+import io.bartmilo.database.TestDataUtil;
+import io.bartmilo.database.domain.Book;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import static org.mockito.Mockito.eq;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class BookDaoImplTests {
+    @Mock
+    private JdbcTemplate jdbcTemplateMock;
+    @InjectMocks
+    private BookDaoImpl underTest;
+
+    @Test
+    public void testThatCreateBookGeneratesCorrectSQL() {
+        Book book = TestDataUtil.createSingleTestBook();
+
+        underTest.create(book);
+
+        verify(jdbcTemplateMock).update(
+                eq("INSERT INTO books (isbn, title, author_id) VALUES (?, ?, ?)"),
+                eq("978-83-64640-14-8"),
+                eq("Elementor Fundamentals"),
+                eq(1L)
+        );
+    }
+
+    @Test
+    public void testThatFindOneBookGeneratesCorrectSQL() {
+        underTest.findOne("978-83-64640-14-8");
+
+        verify(jdbcTemplateMock).query(
+                eq("SELECT * FROM books WHERE isbn = ? LIMIT 1"),
+                ArgumentMatchers.<BookDaoImpl.BookRowMapper>any(),
+                eq("978-83-64640-14-8")
+        );
+    }
+
+    @Test
+    public void testThatFindListOfBooksGeneratesTheCorrectSql() {
+        underTest.findAll();
+
+        verify(jdbcTemplateMock).query(
+                eq("SELECT * FROM books"),
+                ArgumentMatchers.<BookDaoImpl.BookRowMapper>any()
+        );
+    }
+}
