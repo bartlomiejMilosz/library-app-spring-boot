eea7cfee9f533534b16a213f3290559f3a92ab13
Bart Milo <bartlomiej.milosz@gmail.com>
2024-03-16 13:09:25 +0100

    Initialize CRUD DAO interfaces for Book and Author

---
+++ src/main/java/io/bartmilo/database/dao/BookDao.java
@@ -0,0 +1,14 @@
+package io.bartmilo.database.dao;
+
+import io.bartmilo.database.domain.Book;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface BookDao {
+    void create(Book book);
+
+    Optional<Book> findOne(String isbn);
+
+    List<Book> findAll();
+}
